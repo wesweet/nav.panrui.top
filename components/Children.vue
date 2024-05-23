d7ec27421fcec9f199bded15dba10103a268b430
panr99 <1547177202@qq.com>
2024-05-23 10:38:56 +0800

    添加vue测试页面

---
+++ components/Children.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-row :gutter="20" class="form-row">
+    <el-col :span="10"
+      ><el-button type="primary" @click="handleEditAge"
+        >修改年龄</el-button
+      ></el-col
+    >
+  </el-row>
+</template>
+
+<script lang="ts" setup>
+const form: any = defineModel();
+
+const handleEditAge = () => {
+  form.value.age = Math.floor(Math.random() * 100);
+};
+</script>
+
+<style lang="less" scoped>
+.form-row {
+  margin-bottom: 10px;
+}
+</style>
---
+++ pages/demo.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container>
+    <el-header></el-header>
+    <el-main>
+      <el-row>
+        <el-col :span="4">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix" style="margin-bottom: 20px">
+              <span>测试vue3.4 defineModel 属性</span>
+            </div>
+            <el-row :gutter="20" class="form-row">
+              <el-col :span="6">年龄：</el-col>
+              <el-col :span="6">{{ form.age }}</el-col>
+              <el-col :span="6"> <Children v-model="form" /> </el-col>
+            </el-row>
+          </el-card>
+        </el-col>
+        <el-col :span="4"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+const form = ref({
+  age: 18,
+});
+</script>
+
+<style lang="less" scoped></style>
