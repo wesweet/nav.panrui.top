425ef286c97155fd4abc021a1466b369f03c9c6b
panr99 <1547177202@qq.com>
2024-09-12 11:12:18 +0800

    feat：生成随机数

--- pages/generateRandomNumber.vue
+++ pages/generateRandomNumber.vue
@@ -1,7 +1,13 @@
 <template>
   <el-container>
     <el-header>
-      <el-row :gutter="20" type="flex" class="row-bg" justify="space-between" style="margin-top:20px">
+      <el-row
+        :gutter="20"
+        type="flex"
+        class="row-bg"
+        justify="space-between"
+        style="margin-top: 20px"
+      >
         <el-col :span="6">
           <el-input
             v-model="currentRandomNumber"
@@ -13,7 +19,7 @@
             >生成随机数</el-button
           ></el-col
         >
-        <el-col :span="6">
+        <!-- <el-col :span="6">
           <el-select
             v-model="searchVal"
             placeholder="查看已生成的数据"
@@ -27,6 +33,21 @@
             >
             </el-option>
           </el-select>
+        </el-col> -->
+      </el-row>
+      <el-row
+        :gutter="20"
+        type="flex"
+        class="row-bg"
+        justify="space-between"
+        style="margin-top: 20px"
+      >
+        <el-col :span="24">
+          <div style="overflow-wrap: break-word">
+            <span v-for="item in randomNumbers" style="margin: 0 5px">{{
+              item
+            }}</span>
+          </div>
         </el-col>
       </el-row>
     </el-header>
@@ -50,24 +71,25 @@ onMounted(() => {
 const currentRandomNumber: any = ref(0);
 const searchVal: any = ref("");
 const generateRandomNumber = () => {
-  const min = 0.05;
-  const max = 5;
-  // 生成一个介于0和1之间的随机数
-  let randomNumber = Math.random();
-  // 缩放这个随机数到介于min和max之间
-  randomNumber = min + (max - min) * randomNumber;
-  // 保留两位小数
-  randomNumber = parseFloat(randomNumber.toFixed(2));
-  // 判断生成的随机数是否已经存在，如果存在则重新生成随机数
-  while (randomNumbers.value.includes(randomNumber)) {
-    randomNumber = Math.random();
+  for (let index = 0; index < 100; index++) {
+    const min = 0.05;
+    const max = 5;
+    // 生成一个介于0和1之间的随机数
+    let randomNumber = Math.random();
+    // 缩放这个随机数到介于min和max之间
     randomNumber = min + (max - min) * randomNumber;
+    // 保留两位小数
     randomNumber = parseFloat(randomNumber.toFixed(2));
+    // 判断生成的随机数是否已经存在，如果存在则重新生成随机数
+    while (randomNumbers.value.includes(randomNumber)) {
+      randomNumber = Math.random();
+      randomNumber = min + (max - min) * randomNumber;
+      randomNumber = parseFloat(randomNumber.toFixed(2));
+    }
+    currentRandomNumber.value = randomNumber;
+    randomNumbers.value.push(randomNumber);
   }
-  currentRandomNumber.value = randomNumber;
-  randomNumbers.value.push(randomNumber);
   // 将randomNumbers存入到缓存当中
-  console.log(window.localStorage);
   if (typeof window !== "undefined" && window.localStorage) {
     window.localStorage.setItem(
       "randomNumbers",
